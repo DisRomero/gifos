9bf171827163b9c32e7929359da540f92af98ffa
Diana Romero <diana.romero@teaminternational.com>
2021-04-21 20:23:16 -0500

    Create the HTML files of the menu options

--- js/index.js
+++ js/index.js
@@ -15,87 +15,86 @@ function checkMode() {
 
 function darkModeOn() {
   document.body.classList.add("dark-mode");
-  localStorage.setItem('dark-mode', 'true');
+  localStorage.setItem("dark-mode", "true");
 
-// replace text mode
-  const txtMode = document.getElementById('text-color-switch');
-  txtMode.innerHTML = txtMode.innerHTML.replace('nocturno','diurno');
+  // replace text mode
+  const txtMode = document.getElementById("text-color-switch");
+  txtMode.innerHTML = txtMode.innerHTML.replace("nocturno", "diurno");
 }
 
 function darkModeOff() {
   document.body.classList.remove("dark-mode");
-  localStorage.setItem('dark-mode', 'false');
-// replace text mode
-  const txtMode = document.getElementById('text-color-switch');
-  txtMode.innerHTML = txtMode.innerHTML.replace('diurno','nocturno');
+  localStorage.setItem("dark-mode", "false");
+  // replace text mode
+  const txtMode = document.getElementById("text-color-switch");
+  txtMode.innerHTML = txtMode.innerHTML.replace("diurno", "nocturno");
 }
 
 // get value of dark-mode into local storage
-if(localStorage.getItem('dark-mode') === 'true'){
+if (localStorage.getItem("dark-mode") === "true") {
   colorSwitch.checked = true;
   darkModeOn();
-}else{
-  colorSwitch.classList.remove('active');
+} else {
+  colorSwitch.classList.remove("active");
   darkModeOff();
 }
 
-
 //fetch search input
-const inputSearch = document.getElementById('input-form-search').value;
-const btnSearch = document.getElementById('btn-form-search');
+const inputSearch = document.getElementById("input-form-search").value;
+const btnSearch = document.getElementById("btn-form-search");
 
-const q = 'smile';
+const q = "smile";
 const api_key = "I9YUl0qQ7GUVk9LXsawA8eFHyjZC7HRP";
 //const api_key = 'M56ORmffhkS4OWzdIE3ZPfFQXWSjF30N';
 const api_search = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&limit=6`;
 
-
-btnSearch.addEventListener('click', (event)=>{
+btnSearch.addEventListener("click", (event) => {
   event.preventDefault();
   sendApiRequest();
-})
+});
 
-function sendApiRequest(){
-  fetch(api_search).then(function(respondeSearch){
-    console.log(respondeSearch);
-    return respondeSearch.json();
-    
-  }).then(function(json){
-    console.log(json)
-    json.data.forEach(function(obj){
-      console.log(obj.images.downsized.url);
+function sendApiRequest() {
+  fetch(api_search)
+    .then(function (respondeSearch) {
+      console.log(respondeSearch);
+      return respondeSearch.json();
     })
-  }).catch(function(err){
-    console.log(err.message);
-  })
+    .then(function (json) {
+      console.log(json);
+      json.data.forEach(function (obj) {
+        console.log(obj.images.downsized.url);
+      });
+    })
+    .catch(function (err) {
+      console.log(err.message);
+    });
 }
 
 //fetch title trending
 const trendingTitles = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=2`;
 
 fetch(trendingTitles)
-.then(function(respondeTrending){
-  return respondeTrending.json();
-}).then(function(json){
-  json.data.forEach(function(obj){
-    console.log(obj.title);
-    let titleTrending = obj.title;
-    let tag = document.createElement('p');
-    let textInfo = document.createTextNode(titleTrending+',');
-    tag.appendChild(textInfo);
-    let tagSection = document.getElementById('trending-info');
-    tagSection.appendChild(tag);
+  .then(function (respondeTrending) {
+    return respondeTrending.json();
   })
-}).catch(function(err){
-  console.error('The API responde of Trending Titles display an error');
-  console.log(err.message);
-})
-
-
+  .then(function (json) {
+    json.data.forEach(function (obj) {
+      console.log(obj.title);
+      let titleTrending = obj.title;
+      let tag = document.createElement("span");
+      let textInfo = document.createTextNode(titleTrending + ",");
+      tag.appendChild(textInfo);
+      let tagSection = document.getElementById("trending-info");
+      tagSection.appendChild(tag);
+    });
+  })
+  .catch(function (err) {
+    console.error("The API responde of Trending Titles display an error");
+    console.log(err.message);
+  });
 
 /////fetch gif treding
-const trending =
-`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=1`;
+const trending = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=1`;
 
 fetch(trending)
   .then((response) => response.json())
@@ -105,11 +104,11 @@ fetch(trending)
     let url_img = data.data[0].images.downsized_large.url;
     //console.log(url_img);
     let element = document.getElementById("test");
-   // element.innerHTML = '<img src="' + url_img + '  height="200" width="200"/>';
+    // element.innerHTML = '<img src="' + url_img + '  height="200" width="200"/>';
   })
   .catch((err) => console.log(err));
 
-  
-  ////////search responde  
-  const sectionTag = document.getElementById('search-gifos-responde').style.display ='none';
- 
+////////search responde
+const sectionTag = (document.getElementById(
+  "search-gifos-responde"
+).style.display = "none");
