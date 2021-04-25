add88c2f14748633c143f5714a04ccbe075543f4
Diana Romero <33766030+DisRomero@users.noreply.github.com>
2021-04-24 23:14:51 -0500

    add the suggest option of the input

--- js/searchGifos.js
+++ js/searchGifos.js
@@ -1,35 +1,39 @@
-//fetch search input
-
-let inputSearch = "";
 let q = "";
 let api_search = "";
+let api_suggestion ="";
+let inputSearch ="";
 const btnSearch = document.getElementById("btn-form-search");
 const api_key = "I9YUl0qQ7GUVk9LXsawA8eFHyjZC7HRP";
 //const api_key = 'M56ORmffhkS4OWzdIE3ZPfFQXWSjF30N';
+const search = document.getElementById('input-form-search');
+const matchList =document.getElementById('match-list');
 
+
+//// click input delete value
 btnSearch.addEventListener("click", (event) => {
   event.preventDefault();
   getValues();
   sendApiRequest();
-});
+  });
 
 function getValues() {
   inputSearch = document.getElementById("input-form-search").value;
-  const q = inputSearch;
+  q = inputSearch;
   api_search = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&limit=1`;
-  console.log("valor de api_search " + api_search);
+  api_suggestion = `https://api.giphy.com/v1/gifs/search/tags?api_key=${api_key}&q=${q}&limit=1`;
 }
 
-export function sendApiRequest() {
+export async function sendApiRequest() {
   fetch(api_search)
     .then(function (respondeSearch) {
-      console.log(respondeSearch);
+      console.log(respondeSearch);      
       return respondeSearch.json();
     })
     .then(function (json) {
-      console.log(json);
+      ///////////respondeJson = json;
+      //console.log(json);
       json.data.forEach(function (obj) {
-        console.log(obj.images.downsized.url);
+        //console.log(obj.images.downsized.url);
       });
     })
     .catch(function (err) {
@@ -37,7 +41,30 @@ export function sendApiRequest() {
     });
 }
 
+const searchGifos = async searchText => {
+  const res = await fetch (`https://api.giphy.com/v1/gifs/search/tags?api_key=${api_key}&q=${searchText}&limit=2`);
+  const gifos = await res.json();
+  //console.log(gifos);
+  //data.data[0].name
+  gifos.data.forEach(function (obj){
+    //console.log(obj.name);
+  
+let nameSuggestion = obj.name;
+let tag = document.createElement('li');
+let textSuggestion =document.createTextNode(nameSuggestion);
+tag.appendChild(textSuggestion);
+let tagForm = document.getElementById('ul-form-search');
+tagForm.appendChild(tag);
+if(searchText.length === 0){
+  tag.style.display="none";
+}
+  });
+  
 
-////////https://www.youtube.com/watch?v=bRdjEpodiaQ&t=222s&ab_channel=TheTechTeam
-///////https://www.youtube.com/watch?v=1iysNUrI3lw&t=78s&ab_channel=TraversyMedia
-///////https://www.youtube.com/watch?v=WI0aCIEYXvw&ab_channel=J%26GProyectosWeb
+
+  //if(searchText.length === 0)
+
+  //get matches to current text input
+};
+
+search.addEventListener('input', ()=> searchGifos(search.value));
