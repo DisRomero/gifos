5a2872cd514c17c5f4a7363c2567de944f97e507
Diana Romero <diana.romero@teaminternational.com>
2021-04-28 19:53:24 -0500

    add the message when the search doesnt found any gifs

--- js/searchGifos.js
+++ js/searchGifos.js
@@ -1,24 +1,24 @@
 let q = "";
 let api_search = "";
-let api_suggestion ="";
-let inputSearch ="";
+//let api_suggestion ="";
+let inputSearch = "";
 const api_key = "I9YUl0qQ7GUVk9LXsawA8eFHyjZC7HRP";
-const search = document.getElementById('input-form-search');
+const search = document.getElementById("input-form-search");
 const btnSearch = document.getElementById("btn-form-search");
-const suggestion = document.getElementById('ul-form-search');
-const divResponde = document.getElementById('search-gifos-responde');
-const titleResponde = document.getElementById('tittle-search-gifos-responde');
-const listResponde = document.getElementById('ul-search-gifos-responde');
+const suggestion = document.getElementById("ul-form-search");
+const divResponde = document.getElementById("search-gifos-responde");
+const titleResponde = document.getElementById("tittle-search-gifos-responde");
+const listResponde = document.getElementById("ul-search-gifos-responde");
 
 ////////search responde
-const sectionTag = divResponde.style.display = "none";
+const sectionTag = (divResponde.style.display = "none");
 
 btnSearch.addEventListener("click", (event) => {
   event.preventDefault();
   getValues();
   sendApiRequest();
-  suggestion.style.display='none';
-  });
+  suggestion.style.display = "none";
+});
 
 function getValues() {
   inputSearch = document.getElementById("input-form-search").value;
@@ -32,26 +32,44 @@ export async function sendApiRequest() {
     .then(function (respondeSearch) {
       return respondeSearch.json();
     })
-    .then(function (json) {
-      if(json.data.length===0){
-        suggestion.style.display='none';
-        console.log('perdon no se encontro datos');
-      };     
-      json.data.forEach(function (obj) {
-      /////responde and display the div
-      divResponde.style.display='flex'; 
-      /////display the title     
-      let textSuggestion = document.createTextNode(q);
-      titleResponde.innerHTML = textSuggestion.data;
-      ////create and display the gif's into a list
-      let tagLiResponde = document.createElement('li');
-      let tagImgResponde = document.createElement('img');
-      tagImgResponde.src = obj.images.downsized.url;
-      listResponde.appendChild(tagLiResponde);
-      tagLiResponde.appendChild(tagImgResponde);
-     // add the function Ver mas
 
-/**
+    .then(function (json) {
+      if (json.data.length === 0) {
+        suggestion.style.display = "none";
+        listResponde.innerHTML = "";
+        let btnSeeMore = document.getElementById("btn-search-gifos-responde");
+        listResponde.style.display = "none";
+        btnSeeMore.style.display = "none";
+
+        //add the fail information/responde
+        divResponde.style.display = "flex";
+        let textSuggestion = document.createTextNode(q);
+        titleResponde.innerHTML = textSuggestion.data;
+        let imgFailResponde = document.createElement("img");
+        //imgFailResponde.classList.add("claseDePrueba");
+        let txtFailResponde = document.createElement("p");
+        txtFailResponde.classList.add("txt-fail-responde");
+        imgFailResponde.src = "assets/img/icon-busqueda-sin-resultado.svg";
+        txtFailResponde.innerHTML = "Intenta con otra búsqueda.";
+        divResponde.appendChild(imgFailResponde);
+        divResponde.appendChild(txtFailResponde);
+      }
+
+      listResponde.innerHTML = "";
+      json.data.forEach(function (obj) {
+        /////responde and display the information
+        divResponde.style.display = "flex";
+        let textSuggestion = document.createTextNode(q);
+        titleResponde.innerHTML = textSuggestion.data;
+        ////create and display the gif's into a list
+        let tagLiResponde = document.createElement("li");
+        let tagImgResponde = document.createElement("img");
+        tagImgResponde.src = obj.images.downsized.url;
+        listResponde.appendChild(tagLiResponde);
+        tagLiResponde.appendChild(tagImgResponde);
+        // add the function Ver mas
+
+        /**
  * listResponde.appenchild(tag creado)
  * 
  * var img = document.createElement("img");
@@ -65,20 +83,21 @@ src.appendChild(img);
     .catch(function (err) {
       console.log(err.message);
     });
-};
+}
 
-const searchGifos = async searchText => {
-  const res = await fetch 
-  (`https://api.giphy.com/v1/gifs/search/tags?api_key=${api_key}&q=${searchText}&limit=4`);
+const searchGifos = async (searchText) => {
+  const res = await fetch(
+    `https://api.giphy.com/v1/gifs/search/tags?api_key=${api_key}&q=${searchText}&limit=4`
+  );
   const gifos = await res.json();
-  suggestion.innerHTML = '';
+  suggestion.innerHTML = "";
 
-  gifos.data.forEach(function (obj){
-    let tagImg = document.createElement('img');
-    let tag = document.createElement('li');
+  gifos.data.forEach(function (obj) {
+    let tagImg = document.createElement("img");
+    let tag = document.createElement("li");
     let textSuggestion = document.createTextNode(obj.name);
-    
-    tag.addEventListener('click', (e) =>{
+
+    tag.addEventListener("click", () => {
       search.value = obj.name;
       btnSearch.click();
     });
@@ -87,18 +106,17 @@ const searchGifos = async searchText => {
     suggestion.appendChild(tagImg);
     suggestion.appendChild(tag);
   });
-
 };
 
-search.addEventListener('keyup', (e) => {
+search.addEventListener("keyup", (e) => {
   searchGifos(e.target.value);
 
-  if(e.target.value === ''){
-    suggestion.style.display='none';
-    suggestion.value = ''; 
-  }else{
-    suggestion.style.display='block';
-  };
+  if (e.target.value === "") {
+    suggestion.style.display = "none";
+    suggestion.value = "";
+  } else {
+    suggestion.style.display = "block";
+  }
 });
 
 ////// disparar busqueda con enter
