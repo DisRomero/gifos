b65634c2a7cfb5c3412c97e9bc0e9f338805feac
Diana Romero <33766030+DisRomero@users.noreply.github.com>
2021-05-02 23:56:04 -0500

    create the fucntions to display the information when the api responde with not found

--- js/searchGifos.js
+++ js/searchGifos.js
@@ -1,15 +1,11 @@
 let q = "";
 let api_search = "";
-//let api_suggestion ="";
 let inputSearch = "";
 const api_key = "I9YUl0qQ7GUVk9LXsawA8eFHyjZC7HRP";
 const search = document.getElementById("input-form-search");
 const btnSearch = document.getElementById("btn-form-search");
 const suggestion = document.getElementById("ul-form-search");
 const divResponde = document.getElementById("search-gifos-responde");
-const titleResponde = document.getElementById("tittle-search-gifos-responde");
-const listResponde = document.getElementById("ul-search-gifos-responde");
-
 
 btnSearch.addEventListener("click", (event) => {
   event.preventDefault();
@@ -22,22 +18,67 @@ function getValues() {
   inputSearch = document.getElementById("input-form-search").value;
   q = inputSearch;
   api_search = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&offset=4&limit=6`;
-  //api_suggestion = `https://api.giphy.com/v1/gifs/search/tags?api_key=${api_key}&q=${q}&limit=1`;
-}
+};
 
 function addGifo (gifo) {
-    /////responde and display the information
+  const titleResponde = document.getElementById("tittle-search-gifos-responde");
+  const listResponde = document.getElementById("ul-search-gifos-responde");
+
     divResponde.style.display = "flex";
     titleResponde.innerText = q;
-    ////create and display the gif's into a list
     let tagLiResponde = document.createElement("li");
     let tagImgResponde = document.createElement("img");
     tagImgResponde.src = gifo.images.downsized.url;
     tagLiResponde.appendChild(tagImgResponde);
     listResponde.appendChild(tagLiResponde);
+};
+
+function respondeWithoutGifos (){
+  divResponde.innerHTML = '';
+  const divLine = document.createElement('div');
+  divLine.classList.add('line');
+  const title = document.createElement('h2');
+  title.classList.add('text-body');
+  title.innerText = q;
+  const img = document.createElement('img');
+  img.src = "assets/img/icon-busqueda-sin-resultado.svg";
+  const p = document.createElement('p');
+  p.innerText = 'Intenta con otra búsqueda.';
+  p.classList.add('txt-fail-responde');
+  divResponde.appendChild(divLine);
+  divResponde.appendChild(title);
+  divResponde.appendChild(img);
+  divResponde.appendChild(p);
+};
+
+function respondeWithGifos (gifo){
+  divResponde.innerHTML = '';
+  const divLine = document.createElement('div');
+  divLine.classList.add('line');
+  const title = document.createElement('h2');
+  title.classList.add('text-body');
+  title.innerText = q;
+  const ul = document.createElement('ul');
+  ul.classList.add('responde-list');
+  divResponde.appendChild(divLine);
+  divResponde.appendChild(title);
+  divResponde.appendChild(ul);
+
+  const btn = document.createElement('button');
+  btn.classList.add('text-body');
+  btn.textContent = 'ver más';
+  divResponde.appendChild(btn);
+
+  // const li = document.createElement('li');
+  // const img = document.createElement('img');
+  // img.src = gifo.images.downsized.url;
+  // li.appendChild(img);
+  // ul.appendChild(li);
+  // divResponde.appendChild(ul);
+
 }
 
-// 
+
 export async function sendApiRequest() {
   fetch(api_search)
     .then(function (respondeSearch) {
@@ -45,41 +86,47 @@ export async function sendApiRequest() {
     })
     .then(function (json) {
       const data = json.data;
-      let btnSeeMore = document.getElementById("btn-search-gifos-responde");
+      let btnSeeMore = document.getElementById("btn-search-gifos-responde");//
 
       if(data.length === 0) {
-        listResponde.innerHTML = "";
-        btnSeeMore.style.display = "none";
-
-        //add the fail information/responde
-        divResponde.style.display = "flex";
-        ////create and display the gif's into a list
-        let tagLiResponde = document.createElement("li");
-        let tagImgResponde = document.createElement("img");
-        let txtFailResponde = document.createElement("p");
-        txtFailResponde.classList.add("txt-fail-responde");
-        txtFailResponde.innerText = "Intenta con otra búsqueda.";
-        tagImgResponde.src = "assets/img/icon-busqueda-sin-resultado.svg";
-        tagLiResponde.appendChild(tagImgResponde);
-        tagLiResponde.appendChild(txtFailResponde);
-        titleResponde.innerHTML = q;
-        listResponde.appendChild(tagLiResponde);
+        respondeWithoutGifos();
       }
 
       if(data.length > 0) {
-        btnSeeMore.style.display = 'block';
-        listResponde.innerHTML = "";
-        let initialPosition = 0;
-        let size = 4;
-        let jsonData = data.splice(initialPosition, size);
-        jsonData.forEach(addGifo);
-        btnSeeMore.addEventListener('click', () => {
-          let nextGifos = data.splice(initialPosition, size);
-          nextGifos.forEach(addGifo);
-          if(data.length === 0) {
-            btnSeeMore.style.display = 'none';
-          }
-        })
+        divResponde.innerHTML = '';
+        respondeWithGifos();
+
+        data.forEach( function(e){
+          const li = document.createElement('li');
+          const img = document.createElement('img');
+          img.src = e.images.downsized.url;
+          li.appendChild(img);
+          ul.appendChild(li);//add if of the responde with gifos function to call here
+          divResponde.appendChild(ul);
+
+
+        });
+
+        
+
+
+
+        //<button class="text-body" id="btn-search-gifos-responde">ver más</button>
+
+
+        // btnSeeMore.style.display = 'block';
+        // listResponde.innerHTML = "";
+        // let initialPosition = 0;
+        // let size = 4;
+        // let jsonData = data.splice(initialPosition, size);
+        // jsonData.forEach(addGifo);
+        // btnSeeMore.addEventListener('click', () => {
+        //   let nextGifos = data.splice(initialPosition, size);
+        //   nextGifos.forEach(addGifo);
+        //   if(data.length === 0) {
+        //     btnSeeMore.style.display = 'none';
+        //   }
+        // })
       }
 
     })
